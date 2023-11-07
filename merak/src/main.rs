364faa634d5ae5b3a55a4b0d47f0f5ca60c5581e
gsxhnd <gsxhnd@gmail.com>
2023-11-07 21:48:40 +0800

    update

--- merak/src/main.rs
+++ merak/src/main.rs
@@ -1,3 +1,40 @@
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
 fn main() {
+    let conn = Connection::open("./db/test.db").expect("open db error");
+    let _ = conn.execute(
+        "CREATE TABLE person (
+            id    INTEGER PRIMARY KEY,
+            name  TEXT NOT NULL,
+            data  BLOB
+        )",
+        (), // empty list of parameters.
+    );
+
+    let mut stmt = conn
+        .prepare("SELECT id, name, data FROM person")
+        .expect("select error");
+
+    let person_iter = stmt
+        .query_map([], |row| {
+            Ok(Person {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                data: row.get(2)?,
+            })
+        })
+        .expect("");
+
+    for person in person_iter {
+        // person.unwrap();
+        println!("Found person {:?}", person.unwrap());
+    }
     println!("Hello, world!");
 }
