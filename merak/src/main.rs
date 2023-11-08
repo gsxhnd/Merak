73ad9dd815c00122643d2ad653b90cdcd9fbdb49
gsxhnd <gsxhnd@gmail.com>
2023-11-08 20:19:21 +0800

    init

--- merak/src/main.rs
+++ merak/src/main.rs
@@ -1,40 +1,19 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router, Server,
+};
 use rusqlite::{Connection, Result};
+use std::net::SocketAddr;
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
-}
-
-fn main() {
-    let conn = Connection::open("./db/test.db").expect("open db error");
-    let _ = conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
-            data  BLOB
-        )",
-        (), // empty list of parameters.
-    );
-
-    let mut stmt = conn
-        .prepare("SELECT id, name, data FROM person")
-        .expect("select error");
-
-    let person_iter = stmt
-        .query_map([], |row| {
-            Ok(Person {
-                id: row.get(0)?,
-                name: row.get(1)?,
-                data: row.get(2)?,
-            })
-        })
-        .expect("");
-
-    for person in person_iter {
-        // person.unwrap();
-        println!("Found person {:?}", person.unwrap());
-    }
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let _conn = Connection::open("./data/test.db").expect("open db error");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
